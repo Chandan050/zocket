820241a539b6650f5b11dbac34cecbf9ea58abfa
Chandan050 <106652740+Chandan050@users.noreply.github.com>
2023-09-05 20:48:50 +0530

    Update producer.go

--- producer/producer.go
+++ producer/producer.go
@@ -1,29 +1,2 @@
-package producer
 
-import (
-	"fmt"
-	"log"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-)
-
-func SendMessageToKafka(producer *kafka.Producer, productID int) {
-	// Produce a message to the Kafka topic
-	topic := "test-topic" //  Kafka topic
-	message := fmt.Sprintf("%d", productID)
-
-	// Produce a message to Kafka
-	err := producer.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-		Value:          []byte(message),
-	}, nil)
-
-	if err != nil {
-		log.Printf("Failed to send message to Kafka: %v", err)
-	} else {
-		fmt.Println(message)
-	}
-	producer.Flush(15 * 100)
-	producer.Close()
-
-}
